2b9cb01cd4aa8c7e41d2dbe1fdb0529d501e5bd9
Robert Aldrich <robbielaldrich@gmail.com>
2016-06-28 09:23:39 -0700

    monkey patching works -- need to reset viewed card

--- Notepad.py
+++ Notepad.py
@@ -2,8 +2,7 @@ notepad_shortcut = '|'
 notepad_on_start = 0 #switch to 1 for notepad by default
 notepad_bool = notepad_on_start
 
-"""If find problem, email at robbie9889@gmail.com [this should be at very top]"""
-
+"""If find problem, email at robbielaldrich@gmail.com"""
 
 __addon_name__ = "Notepad"
 __version__ = "0.0"
@@ -16,29 +15,31 @@ from aqt.utils import showInfo
 from aqt.qt import *
 from anki.hooks import wrap
 from anki.collection import _Collection
+from aqt.reviewer import Reviewer 
+
+def npRenderQA(self, data, qfmt=None, afmt=None):
+    d = origRenderQA(self, data, qfmt, afmt)
+
+    #CUSTOM CODE
+    d['q'] += ("<br/><input>")
+    return d
 
 # anki/collection.py
 # _renderQA(data, *args) called by _getQA() in anki/cards.py
-def npRenderQA(self, data, qfmt=None, afmt=None):
-    origRenderQA(self, data, qfmt, afmt)
-
-    #CUSTOM CODE
-    d['q'] += ("<input>")
-    return d
-
 origRenderQA = _Collection._renderQA
 
 def keyHandler(self, evt, _old):
 	global notepad_bool
-    key = unicode(evt.text())
-    if key == notepad_shortcut:
-    	if notepad_bool == 0:
-    		notepad_bool = 1
-    		_Collection._renderQA = npRenderQA
-    	else if notepad_bool == 1:
-    		notepad_bool = 0
-    		_Collection._renderQA = origRenderQA
-	else: return _old(self, evt)
+	key = unicode(evt.text())
+	if key == notepad_shortcut:
+		if notepad_bool == 0:
+			notepad_bool = 1
+			_Collection._renderQA = npRenderQA
+		elif notepad_bool == 1:
+			notepad_bool = 0
+			_Collection._renderQA = origRenderQA
+	else: 
+		return _old(self, evt)
 
 
 

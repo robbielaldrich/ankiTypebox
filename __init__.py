c3ed7f12856dcee52cb67d05b667a348bd55e01e
robert_aldrich <robert_aldrich@apple.com>
2020-03-28 15:30:17 -0700

    show answer with ctrl+enter; allow no answer correction

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from aqt.reviewer import Reviewer
 from anki.utils import stripHTML
 
 
-Reviewer.typeboxAnsPat = r"\[\[type:box:(.+?)\]\]"
+Reviewer.typeboxAnsPat = r"\[\[typebox:(.*?)\]\]"
 
 
 def typeboxAnsFilter(self, buf: str) -> str:
@@ -27,27 +27,30 @@ def typeboxAnsQuestionFilter(self, buf: str) -> str:
         return buf
     fld = m.group(1)
     # loop through fields for a match
-    for f in self.card.model()["flds"]:
+    self.typeCorrect = None
+    fields = self.card.model()["flds"]
+    for f in fields:
         if f["name"] == fld:
             # get field value and style details
             self.typeCorrect = self.card.note()[f["name"]]
             self.typeFont = f["font"]
             self.typeSize = f["size"]
             break
-
     if not self.typeCorrect:
-        if self.typeCorrect is None:
-            warn = _("Type answer: unknown field %s") % fld
-            return re.sub(self.typeboxAnsPat, warn, buf)
-        # empty field, remove typebox answer pattern
-        return re.sub(self.typeboxAnsPat, "", buf)
-
+        maybe_answer_field = next((f for f in fields if f == "Back"), fields[-1])
+        self.typeFont = maybe_answer_field["font"]
+        self.typeSize = maybe_answer_field["size"]
     return re.sub(
         self.typeboxAnsPat,
         """
 <center>
-<textarea id=typeans style="font-family: '%s'; font-size: %spx;"></textarea>
+<textarea id=typeans onkeypress="typeboxAns();" style="font-family: '%s'; font-size: %spx;"></textarea>
 </center>
+<script>
+function typeboxAns() {
+    if (window.event.keyCode == 13 && window.event.ctrlKey) pycmd("ans");
+}
+</script>
     """
         % (self.typeFont, self.typeSize),
         buf,
@@ -55,25 +58,29 @@ def typeboxAnsQuestionFilter(self, buf: str) -> str:
 
 
 def typeboxAnsAnswerFilter(self, buf: str) -> str:
-    if not self.typeCorrect:
-        return re.sub(self.typeboxAnsPat, "", buf)
     origSize = len(buf)
     buf = buf.replace("<hr id=answer>", "")
     hadHR = len(buf) != origSize
-    # munge correct value
-    parser = html.parser.HTMLParser()
-    cor = self.mw.col.media.strip(self.typeCorrect)
-    cor = re.sub("(\n|<br ?/?>|</?div>)+", "__newline__", cor)
-    cor = stripHTML(cor)
-    # ensure we don't chomp multiple whitespace
-    cor = cor.replace(" ", "&nbsp;")
-    cor = parser.unescape(cor)
-    cor = cor.replace("\xa0", " ")
-    cor = cor.replace("__newline__", "\n")
-    cor = cor.strip()
+
     given = self.typedAnswer
     # compare with typed answer
-    res = self.correct(given, cor, showBad=False)
+    if self.typeCorrect:
+        # munge correct value
+        parser = html.parser.HTMLParser()
+        cor = self.mw.col.media.strip(self.typeCorrect)
+        cor = re.sub("(\n|<br ?/?>|</?div>)+", "__newline__", cor)
+        cor = stripHTML(cor)
+        # ensure we don't chomp multiple whitespace
+        cor = cor.replace(" ", "&nbsp;")
+        cor = parser.unescape(cor)
+        cor = cor.replace("\xa0", " ")
+        cor = cor.replace("__newline__", "\n")
+        cor = cor.strip()
+
+        res = self.correct(given, cor, showBad=False)
+
+    else:
+        res = self.typedAnswer
 
     # and update the type answer area
     s = """
@@ -86,7 +93,7 @@ def typeboxAnsAnswerFilter(self, buf: str) -> str:
         # a hack to ensure the q/a separator falls before the answer
         # comparison when user is using {{FrontSide}}
         s = "<hr id=answer>" + s
-    return re.sub(self.typeAnsPat, s, buf)
+    return re.sub(self.typeboxAnsPat, s, buf)
 
 
 Reviewer.typeAnsFilter = typeboxAnsFilter
